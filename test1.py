292e8a2186005aa9210324cca3b9296531761b39
swe74-29 <swetahalder29@gmail.com>
2026-07-02 11:12:41 +0530

    made the homepage

--- test1.py
+++ test1.py
@@ -1,1 +1,25 @@
-print("Hello World")
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+@app.route('/DOCS')
+def DOCS():
+   return 'NOT MADE YET!'
+@app.route('/GITHUB')
+def GITHUB():
+   return 'NOT MADE YET!'
+@app.route('/ABOUT')
+def ABOUT():
+   return 'NOT MADE YET!'
+@app.route('/MIT_License')
+def MIT_License():
+   return 'NOT MADE YET!'
+@app.route('/Source_Code')
+def Source_Code():
+   return 'NOT MADE YET!'
+@app.route('/Report_Bug')
+def Report_Bug():
+   return 'NOT MADE YET!'
+if __name__ == "__main__":
+ app.run(debug=True)
