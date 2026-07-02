e630b2be9ffa1d58616e31d317b32a2e1cd26b0d
swe74-29 <swetahalder29@gmail.com>
2026-07-02 12:34:47 +0530

    added recent_scans database

--- test1.py
+++ test1.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///scans.db"
+db=SQLAlchemy(app)
+class ScanHistory(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    ip_address=db.Column(db.String(50), nullable=False)
+    open_ports=db.Column(db.String(200))
+    timestamp=db.Column(db.String(200))
+
 @app.route("/")
 def home_page():
     return render_template('index.html')
+def index():
+    recent_scans = ScanHistory.query.order_by(ScanHistory.id.desc()).limit(10).all()
+    return render_template('index.html', recent_scans=recent_scans)
 @app.route('/DOCS')
 def DOCS():
    return 'NOT MADE YET!'
@@ -23,3 +36,7 @@ def Report_Bug():
    return 'NOT MADE YET!'
 if __name__ == "__main__":
  app.run(debug=True)
+
+
+
+ 
